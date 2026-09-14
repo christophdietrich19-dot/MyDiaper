2ebd35140155fad7d41ad3226124c7d8dbbe6307
christophdietrich19-dot <christophdietrich19@gmail.com>
2026-09-14 23:22:11 +0200

    Add files via upload

--- android/app/src/main/java/de/christophit/mydiaper/MainActivity.java
+++ android/app/src/main/java/de/christophit/mydiaper/MainActivity.java
@@ -1,5 +1,16 @@
 package de.christophit.mydiaper;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebSettings settings = getBridge().getWebView().getSettings();
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+    }
+}
